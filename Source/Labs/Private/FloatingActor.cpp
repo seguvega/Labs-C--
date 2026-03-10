3b6c6f60ef4c8093861ad3abfb92e8a23fd370f4
Sebastián Velasco <seguvega@hotmail.com>
2026-03-10 18:33:16 -0400

    Adding X and Y factor

--- Source/Labs/Private/FloatingActor.cpp
+++ Source/Labs/Private/FloatingActor.cpp
@@ -8,7 +8,10 @@ AFloatingActor::AFloatingActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	AmplitudeX = 20.f;
+	AmplitudeY = 20.f;
+	MovAmplitude = 21.f;
+	XYFactor = 1.f;
 }
 
 // Called when the game starts or when spawned
@@ -24,8 +27,15 @@ void AFloatingActor::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 	FVector NewLocation = GetActorLocation();
 	float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime));
-	NewLocation.Z += DeltaHeight * 20.0f; //Scale our height by a factor of 20
+	NewLocation.Z += DeltaHeight * MovAmplitude; //Scale our height by a factor of 20
 	RunningTime += DeltaTime;
+
+	float NewDeltafactor = DeltaTime * XYFactor;
+
+	// Periodic motion on X and Y
+	NewLocation.X += FMath::Sin(RunningTime) * AmplitudeX * NewDeltafactor;
+	NewLocation.Y += FMath::Cos(RunningTime) * AmplitudeY * NewDeltafactor;
+
 	SetActorLocation(NewLocation);
 }
 
--- Source/Labs/Public/FloatingActor.h
+++ Source/Labs/Public/FloatingActor.h
@@ -16,6 +16,18 @@ public:
 	AFloatingActor();
 
 
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float AmplitudeX;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float AmplitudeY;
+
+	UPROPERTY( EditAnywhere, Category = "Movement")
+	float MovAmplitude;
+
+	UPROPERTY(EditAnywhere, Category = "Movement", meta = (ClampMin = "0.6", ClampMax = "1.4"))
+	float XYFactor;
+
 	virtual void Tick(float DeltaTime) override;
 
 
